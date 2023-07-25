bf17692c28f3676c43b2b7b67772b60baaddcef3
Guilherme Chalita <guilhermechalita@hotmail.com>
2023-07-25 18:36:35 -0300

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#telefone").mask("(00)00000-0000");
+
+  $("form").validate({
+    rules: {
+      nome: { required: true },
+      email: { required: true, email: true },
+      mensagem: { required: true },
+    },
+  });
+});
